df1f357d12eb16201c535868ea7fb209e7385924
cherigra <nikitakrasovskij5@gmail.com>
2023-06-03 09:06:59 +0300

    feat(database): add method select_orders_id_user

--- bot/keyboard_config.py
+++ bot/keyboard_config.py
@@ -14,13 +14,15 @@ def init_keyboard():
 # Заказчик
 def customer_keyboard():
     keyboard = VkKeyboard()
-    keyboard.add_openlink_button('1. Получить ЭЦП', 'https://www.roseltorg.ru/ecp')
+    keyboard.add_openlink_button('Получить ЭЦП', 'https://www.roseltorg.ru/ecp')
     keyboard.add_line()
-    keyboard.add_openlink_button('2. Регистрация в ЕИС', 'https://zakupki.gov.ru/epz/main/public/home.html')
+    keyboard.add_openlink_button('Регистрация в ЕИС', 'https://zakupki.gov.ru/epz/main/public/home.html')
     keyboard.add_line()
-    keyboard.add_button('3. Создание процедуры')
+    keyboard.add_button('Создание процедуры')
     keyboard.add_line()
-    keyboard.add_button('4. Мои заказы')
+    keyboard.add_button('Мои заказы')
+    keyboard.add_line()
+    keyboard.add_button('Услуги')
     keyboard.add_line()
     keyboard.add_button('Вернуться в начало')
     return keyboard
@@ -29,13 +31,15 @@ def customer_keyboard():
 # Поставщик
 def provider_keyboard():
     keyboard = VkKeyboard()
-    keyboard.add_openlink_button('1. Получить ЭЦП', 'https://www.roseltorg.ru/ecp')
+    keyboard.add_openlink_button('Получить ЭЦП', 'https://www.roseltorg.ru/ecp')
     keyboard.add_line()
-    keyboard.add_openlink_button('2. Регистрация в ЕИС', 'https://zakupki.gov.ru/epz/main/public/home.html')
+    keyboard.add_openlink_button('Регистрация в ЕИС', 'https://zakupki.gov.ru/epz/main/public/home.html')
     keyboard.add_line()
-    keyboard.add_button('3. Поиск торгов')
+    keyboard.add_button('Поиск торгов')
     keyboard.add_line()
-    keyboard.add_button('4. Избранные торги')
+    keyboard.add_button('Избранные торги')
+    keyboard.add_line()
+    keyboard.add_button('Услуги')
     keyboard.add_line()
     keyboard.add_button('Вернуться в начало')
     return keyboard
@@ -51,7 +55,7 @@ def back_to_the_start():
 # Возврат в меню заказчика
 def back_to_the_customer_interface():
     keyboard = VkKeyboard()
-    keyboard.add_button('1. Вернуться в меню заказчика')
+    keyboard.add_button('Вернуться в меню заказчика')
     keyboard.add_line()
     keyboard.add_button('Вернуться в начало')
     return keyboard
@@ -60,7 +64,62 @@ def back_to_the_customer_interface():
 # Возврат в меню поставщика
 def back_to_the_provider_interface():
     keyboard = VkKeyboard()
-    keyboard.add_button('1. Вернуться в меню поставщика')
+    keyboard.add_button('Вернуться в меню поставщика')
+    keyboard.add_line()
+    keyboard.add_button('Вернуться в начало')
+    return keyboard
+
+
+# Предложение услуг
+def service_keyboard():
+    keyboard = VkKeyboard()
+    keyboard.add_openlink_button('Электронные подписи', 'https://www.roseltorg.ru/ecp')
+    keyboard.add_line()
+    keyboard.add_openlink_button('Кредит на исполнение контракта', 'https://www.roseltorg.ru/credit-execution-contract')
+    keyboard.add_openlink_button('Помощь в регистрации', 'https://www.roseltorg.ru/speed')
+    keyboard.add_line()
+    keyboard.add_openlink_button('Комплаенс проверка', 'https://www.roseltorg.ru/compliance')
+    keyboard.add_openlink_button('Банковские гарантии', 'https://www.roseltorg.ru/bg')
+    keyboard.add_line()
+    keyboard.add_openlink_button('Сопровождение на торгах', 'https://www.roseltorg.ru/realty-support')
+    keyboard.add_openlink_button('Экспертиза тендерной заявки', 'https://www.roseltorg.ru/bid')
+    keyboard.add_line()
+    keyboard.add_openlink_button('Обучающий центр', 'https://www.roseltorg.ru/education')
+    keyboard.add_openlink_button('Специальные счета', 'https://www.roseltorg.ru/specschet')
+    keyboard.add_line()
+    keyboard.add_openlink_button('ЭДО \"Росинвойс\"', 'https://www.roseltorg.ru/edo')
+    keyboard.add_openlink_button('Центр импортозамещения', 'https://www.roseltorg.ru/import')
+    keyboard.add_line()
+    keyboard.add_openlink_button('Сертификация продукции', 'https://www.roseltorg.ru/certification-center')
+    keyboard.add_openlink_button('Страхование СМР', 'https://www.roseltorg.ru/insurance-fkr')
+    keyboard.add_line()
+    keyboard.add_button('Вернуться в начало')
+    return keyboard
+
+
+# Клавиатура новичка, пробующего разные роли
+def newbie_keyboard():
+    keyboard = VkKeyboard()
+    keyboard.add_button('Я - заказчик', VkKeyboardColor.PRIMARY)
+    keyboard.add_line()
+    keyboard.add_button('Я - поставщик', VkKeyboardColor.PRIMARY)
+    keyboard.add_line()
+    keyboard.add_button('Пройти курс', VkKeyboardColor.PRIMARY)
+    keyboard.add_line()
+    keyboard.add_openlink_button('Чат для новичков', 'http://example.com/')
+    keyboard.add_line()
+    keyboard.add_button('Вернуться в начало')
+    return keyboard
+
+
+# Обучающие курсы
+def newbie_learning_courses():
+    keyboard = VkKeyboard()
+    keyboard.add_openlink_button('Обучающие курсы', 'https://www.roseltorg.ru/education')
+    keyboard.add_line()
+    keyboard.add_openlink_button('Вконтакте', 'http://example.com/')
+    keyboard.add_line()
+    keyboard.add_openlink_button('YouTube', 'http://example.com/')
     keyboard.add_line()
     keyboard.add_button('Вернуться в начало')
     return keyboard
--- database/main.py
+++ database/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/db'
@@ -15,6 +16,15 @@ class Order(db.Model):
     data = db.Column(db.String)
     user_vk_id = db.Column(db.String)
 
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'data': self.data,
+            'user_vk_id': self.user_vk_id
+        }
+
     def __init__(self, name, price, data, user_vk_id):
         self.name = name
         self.price = price
@@ -25,11 +35,18 @@ class Order(db.Model):
         name = order_data[1]
         price = order_data[2]
         data = order_data[3]
-        user_vk_id = order_data[0]
+        user_vk_id = int(order_data[0])
 
         data = Order(name, price, data, user_vk_id)
         db.session.add(data)
         db.session.commit()
+
+    def select_orders_by_user_vk_id(self,user_vk_id):
+        orders = Order.query.filter_by(user_vk_id=user_vk_id).all()
+        orders_json = [order.to_json() for order in orders]
+        return orders_json
         # db.session.refresh()
+
+        # def get order select * from orders As o where o.user_vk_id = inner.user_vk_id
 
 
